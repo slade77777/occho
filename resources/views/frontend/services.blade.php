3de97698c677a66cd9f4154a0c609dea10b1fea9
tung031295 <tung031295@gmail.com>
2017-07-06 22:19:50 +0700

    dá

--- resources/views/frontend/services.blade.php
+++ resources/views/frontend/services.blade.php
@@ -58,57 +58,73 @@
 		</div><!-- /container -->
 	</div><!-- /headerwrap -->
 
+
+	<div class="container">
+		<div class="row mt">
+			<div class="col-lg-8" style="font-family: Helvetica, Arial, sans-serif;">
+				<div>
+
+					<h4 style="font-family: Helvetica, Arial, sans-serif;"><b>1. Các Loại Ứng Dụng Dịch Vụ</b></h4>
+					<p>Thiết Kế và Phát Triển Website</p>
+					<ul>
+						<ol>- Tại nCreation, chúng tôi không chỉ sở hữu một đội ngũ nhân lực chuyên nghiệp trong thi t k , phát triển, và quản lý dự án, mà còn có những kinh nghiệm quý giá và quy trình làm việc linh hoạt có thể đảm bảo chất lượng sản phẩm đạt chuẩn quốc t .  </ol>
+						<ol>- Các ứng dụng Web và Web-enabling,;  ác dịch vụ Web (SOA, REST, SOAP); Xây dựng tương tác giao diện người dung linh hoạt (JavaScript, AJAX, Flex, HTML5, Siliverlight); Hệ thống web có thể dùng cơ sở dữ liệu (MySQL, Oracle, IBM DB2) tùy theo t nh chất của dự án </ol>
+						<ol>- Liên quan tới mảng thi t k  và phát triển website, chúng tôi giúp khách hàng khai thác tối đa tiềm năng của trang web. Thông qua dịch vụ Tối ưu hóa công cụ tìm ki m chi n lược (SEO) và marketing website với công cụ tìm ki n (SEM), chúng tôi sẽ giúp bạn nâng cao vị th  thương hiệu, cải thiện chất lượng trang web, và quan trọng nhất là tăng mức độ truyền thông quảng bá của doanh nghiệp lên một khái niệm và chất lượng mới.</ol>
+					</ul>
+					<p>Nghiên Cứu và ứng dụng phần mềm</p>
+					<ul>
+						<ol>- Ngoài việc xây dựng các gói ứng dụng trên Website, việc xây dựng các ứng dụng ph n mềm chạy trên Windows (dùng công nghệ .NET) hoặc các hệ thống mã nguồn mở trên Unix, Linus (dùng Java) đóng 1 vai trò quan trọng trong việc kinh doanh của công ty </ol>
+						<ol>- Các ứng dụng liên quan tới xử lý sâu trong hệ thống như:  ,  ++ cũng là th  mạnh với công ty do các nhân sự ngành  ông nghệ thông tin được đào tạo căn bản, chuyên sâu về lập trình có thể xây dựng các hệ thống lõi, xử lý ph n cứng</ol>
+						<ol>- Các hệ thống liên quan tới nền tảng di động: ứng dụng điều khiển, game </ol>
+					</ul>
+					<p>Tư Vấn Kĩ thuật,kiểm định phần mềm và hỗ trợ phần mềm</p>
+					<ul>
+						<ol>- Đánh giá hiện trạng hệ thống  ông nghệ thông tin </ol>
+						<ol>- Tư vấn, lập k  hoạch xây dựng, đánh giá chất lượng hệ thống mới </ol>
+					</ul>
+				</div>
+
+				<div>
+					<h4 style="font-family: Helvetica, Arial, sans-serif;"><b>2. Nền Tảng Kĩ Thuật</b></h4>
+					<p><b>Ngôn Ngữ Lập Trình</b></p>
+					<ul>
+						<ol>- PHP: Symphony, Smarty, Mojavi, Pear, ADODB </ol>
+						<ol>- Java: JSP/Servlet, Struts, Hibernate, Velocity, J2EE, BEA Weblogic, JBoss, MIDP, J2ME </ol>
+						<ol>- JavaScript：Ajax, Prototype, Web2.0 </ol>
+						<ol>- Ruby: Ruby On Rails </ol>
+						<ol>- .NET: C#.NET , ASP.NET, ASP.NET, SubSonic Framework </ol>
+						<ol>- iPhone, android </ol>
+						<ol>- Khác: HTML5, Perl/CGI, ColdFusion </ol>
+					</ul>
+					<p><b>Hệ Điều Hành</b>: Windows, Linux, SunSolaris, Mac </p>
+					<p><b>Database</b>: Oracle, SQLServer, MySQL, PostgreSQL  </p>
+					<p><b>Công Cụ Phát Triền</b>:  Eclipse, VisualStudio.NET, DB Designer   </p>
+					<p><b>Công Cụ Quản Lý</b>:   MSProject, Bug tracker, CVS, SVN, Work tracking, CMS    </p>
+				</div>
+				<div>
+					<h4 style="font-family: Helvetica, Arial, sans-serif;"><b>3.Quy Trình Quản Lý</b></h4>
+					<p>Ứng dụng quy trình quản lý Rational Unifield Process (tương đương  MM3) vào trong tất cả các dự án  ông nghệ thông tin của công ty. Ngoài ra việc quản lý k t hợp với các công cụ và quy trình về: </p>
+					<ul>
+						<ol>- Quản lý dự án (MS project), quản lý công việc (Work tracking), quản lý nhân sự (HRM) </ol>
+						<ol>- Quản lý khách hàng, quản lý yêu của Khách Hàng </ol>
+						<ol>- Quản lý văn bản, phiên bản </ol>
+					</ul>
+				</div>
+			</div><!-- col-lg-8 -->
+			<div class="col-lg-4">
+				<ul class="process effect-2" id="process">
+					<li><img src="assets/img/process/pro01.png"></li>
+					<li><img src="assets/img/process/pro02.png"></li>
+					<li><img src="assets/img/process/pro03.png"></li>
+					<li><img src="assets/img/process/pro04.png"></li>
+				</ul>
+			</div><!-- col-lg-4 -->
+		</div><!-- /row -->
+	</div><!-- /.container -->
 	<!-- PROCESS SECTION -->
 	<div class="content" style="font-family: Helvetica, Arial, sans-serif;">
     <div class="container">
-		<div>
-			
-      <h4 style="font-family: Helvetica, Arial, sans-serif;"><b>1. Các Loại Ứng Dụng Dịch Vụ</b></h4>
-			<p>Thiết Kế và Phát Triển Website</p>
-			<ul>
-				<ol>- Tại nCreation, chúng tôi không chỉ sở hữu một đội ngũ nhân lực chuyên nghiệp trong thi t k , phát triển, và quản lý dự án, mà còn có những kinh nghiệm quý giá và quy trình làm việc linh hoạt có thể đảm bảo chất lượng sản phẩm đạt chuẩn quốc t .  </ol>
-				<ol>- Các ứng dụng Web và Web-enabling,;  ác dịch vụ Web (SOA, REST, SOAP); Xây dựng tương tác giao diện người dung linh hoạt (JavaScript, AJAX, Flex, HTML5, Siliverlight); Hệ thống web có thể dùng cơ sở dữ liệu (MySQL, Oracle, IBM DB2) tùy theo t nh chất của dự án </ol>
-				<ol>- Liên quan tới mảng thi t k  và phát triển website, chúng tôi giúp khách hàng khai thác tối đa tiềm năng của trang web. Thông qua dịch vụ Tối ưu hóa công cụ tìm ki m chi n lược (SEO) và marketing website với công cụ tìm ki n (SEM), chúng tôi sẽ giúp bạn nâng cao vị th  thương hiệu, cải thiện chất lượng trang web, và quan trọng nhất là tăng mức độ truyền thông quảng bá của doanh nghiệp lên một khái niệm và chất lượng mới.</ol>
-			</ul>
-			<p>Nghiên Cứu và ứng dụng phần mềm</p>
-			<ul>
-				<ol>- Ngoài việc xây dựng các gói ứng dụng trên Website, việc xây dựng các ứng dụng ph n mềm chạy trên Windows (dùng công nghệ .NET) hoặc các hệ thống mã nguồn mở trên Unix, Linus (dùng Java) đóng 1 vai trò quan trọng trong việc kinh doanh của công ty </ol>
-				<ol>- Các ứng dụng liên quan tới xử lý sâu trong hệ thống như:  ,  ++ cũng là th  mạnh với công ty do các nhân sự ngành  ông nghệ thông tin được đào tạo căn bản, chuyên sâu về lập trình có thể xây dựng các hệ thống lõi, xử lý ph n cứng</ol>
-				<ol>- Các hệ thống liên quan tới nền tảng di động: ứng dụng điều khiển, game </ol>
-			</ul>
-			<p>Tư Vấn Kĩ thuật,kiểm định phần mềm và hỗ trợ phần mềm</p>
-			<ul>
-				<ol>- Đánh giá hiện trạng hệ thống  ông nghệ thông tin </ol>
-				<ol>- Tư vấn, lập k  hoạch xây dựng, đánh giá chất lượng hệ thống mới </ol>
-			</ul>
-		</div>
 
-		<div>
-			<h4 style="font-family: Helvetica, Arial, sans-serif;"><b>2. Nền Tảng Kĩ Thuật</b></h4>
-			<p><b>Ngôn Ngữ Lập Trình</b></p>
-			<ul>
-				<ol>- PHP: Symphony, Smarty, Mojavi, Pear, ADODB </ol>
-				<ol>- Java: JSP/Servlet, Struts, Hibernate, Velocity, J2EE, BEA Weblogic, JBoss, MIDP, J2ME </ol>
-				<ol>- JavaScript：Ajax, Prototype, Web2.0 </ol>
-				<ol>- Ruby: Ruby On Rails </ol>
-				<ol>- .NET: C#.NET , ASP.NET, ASP.NET, SubSonic Framework </ol>
-				<ol>- iPhone, android </ol>
-				<ol>- Khác: HTML5, Perl/CGI, ColdFusion </ol>
-			</ul>
-			<p><b>Hệ Điều Hành</b>: Windows, Linux, SunSolaris, Mac </p>
-			<p><b>Database</b>: Oracle, SQLServer, MySQL, PostgreSQL  </p>
-			<p><b>Công Cụ Phát Triền</b>:  Eclipse, VisualStudio.NET, DB Designer   </p>
-			<p><b>Công Cụ Quản Lý</b>:   MSProject, Bug tracker, CVS, SVN, Work tracking, CMS    </p>
-		</div>
-		<div>
-			<h4 style="font-family: Helvetica, Arial, sans-serif;"><b>3.Quy Trình Quản Lý</b></h4>
-			<p>Ứng dụng quy trình quản lý Rational Unifield Process (tương đương  MM3) vào trong tất cả các dự án  ông nghệ thông tin của công ty. Ngoài ra việc quản lý k t hợp với các công cụ và quy trình về: </p>
-			<ul>
-				<ol>- Quản lý dự án (MS project), quản lý công việc (Work tracking), quản lý nhân sự (HRM) </ol>
-				<ol>- Quản lý khách hàng, quản lý yêu của Khách Hàng </ol>
-				<ol>- Quản lý văn bản, phiên bản </ol>
-			</ul>
-		</div>
     </div><!-- /.container -->
 	</div>
 
